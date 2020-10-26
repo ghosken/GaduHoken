ec3f39d67a5b53606855fb95c9c36281781a17ce
Gadu Hosken <gadu.gameiro@outlook.com>
2020-10-26 18:43:09 -0300

    Inicio

---
+++ GaduHosken/wwwroot/js/util.js
@@ -0,0 +1,15 @@
+﻿function abrirWhatsapp() {
+    window.open("https://api.whatsapp.com/send?phone=5521992339418");
+}
+function abrirLinkedin() {
+    window.open("https://www.linkedin.com/in/gadu-hosken/");
+}
+function abrirFacebook() {
+    window.open("https://www.facebook.com/gadu.linhares/");
+}
+function abrirTwitter() {
+    window.open("https://twitter.com/hoskengadu");
+}
+function abrirGithub() {
+    window.open("https://github.com/ghosken");
+}
